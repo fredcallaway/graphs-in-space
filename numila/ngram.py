19ada9a411d0580418dea86e7dfabfc9533634c5
fredcallaway <fredc@llaway.com>
2016-01-06 15:16:48 -0800

    ngram with SRILM

---
+++ numila/ngram.py
@@ -0,0 +1,90 @@
+"""A wrapper around SRILM"""
+
+import itertools
+import subprocess
+import numpy as np
+import tempfile
+import utils
+import re
+import os
+import shutil
+
+class NGramModel(object):
+    """A wrapper around SRILM"""
+    def __init__(self, order):
+        self.order = order
+        os.makedirs('_srilm/', exist_ok=True)
+        self.dir = tempfile.mkdtemp(dir='_srilm/')  # TODO
+
+    def fit(self, train_data):
+        with utils.Timer('Ngram train time', LOG.info):
+            with open(self.dir + '/train.txt', 'w+') as f:
+                for line in train_data:
+                    f.write(' '.join(line))
+
+            cmd = ('ngram-count '
+                   '-text {self.dir}/train.txt '
+                   '-order {self.order} '
+                   '-lm {self.dir}/model.lm '
+                   #'-kndiscount '  TODO
+                   ).format_map(locals())
+
+            subprocess.check_call(cmd, shell=True)
+            os.remove(self.dir + '/train.txt')
+
+        return self
+
+    def logprob(self, ngram):
+        assert 0
+        ngram = ' '.join(ngram)
+        cmd = ('echo "{ngram}" | '
+               'ngram -order self.order '
+               '-lm {self.dir}/model.lm '
+               '-ppl -'
+               ).format_map(locals())
+
+        subprocess.check_output(cmd, shell=True)
+
+    def speak(self, words, verbose=False):
+        """Returns a single node containing all of `words`."""
+        assert len(words) < 7
+        utts = list(itertools.permutations(words))
+        utts_string = '\n'.join(' '.join(u) for u in utts)
+        cmd = ('echo "{utts_string}" | '
+               'ngram -order {self.order} '
+               '-lm {self.dir}/model.lm '
+               '-debug 1 '
+               '-ppl -'
+               ).format_map(locals())
+
+        out = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        regex = re.compile(rb'\d+ zeroprobs, logprob= \S+ ppl= ([\d.e+]+) ppl1= ')
+
+        perplexities = []
+        test = []
+        for block in out.split(b'\n\n')[:-1]:
+            line = block.rsplit(b'\n', 1)[-1]
+            test.append(block.split(b'\n')[-3])
+            perplexities.append(float(regex.match(line).group(1)))
+        assert len(perplexities) == len(utts)
+
+        best_idx = np.argmin(perplexities)
+        return utts[best_idx]
+
+    def __del__(self):
+        # This is not great practice for cleaning up, but it will have to do.
+        shutil.rmtree(self.dir)
+
+
+
+
+def main():
+    corpus = utils.read_corpus('/usr/local/share/srilm/examples/train.txt')
+    model = NGramModel(3).fit(corpus)
+
+    print(model.speak('acb'))
+
+if __name__ == '__main__':
+    main()
+
+
